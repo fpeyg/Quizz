dcff73f6f356ad19f37e325ee9bc09b411bdaa00
bilalmourdi <58844248+bilalmourdi@users.noreply.github.com>
2020-02-04 14:18:58 +0100

    /

--- src/main/java/com/a84/quizz/quizz/m/User.java
+++ src/main/java/com/a84/quizz/quizz/m/User.java
@@ -15,5 +15,67 @@ public class User {
     private String name;
     private String hashedPassword;
     private String salt;
+    private int accessLevel;
 
-}
+    public int getAccessLevel() {
+        return accessLevel;
+    }
+
+    public void setAccessLevel(int accessLevel) {
+        this.accessLevel = accessLevel;
+    }
+
+    public User() {
+    }
+
+    public User(int user_id, String name, String hashedPassword, String salt, int accessLevel) {
+        this.user_id = user_id;
+        this.name = name;
+        this.hashedPassword = hashedPassword;
+        this.salt = salt;
+        this.accessLevel = accessLevel;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "user_id=" + user_id +
+                ", name='" + name + '\'' +
+                ", hashedPassword='" + hashedPassword + '\'' +
+                ", salt='" + salt + '\'' +
+                '}';
+    }
+
+   }
