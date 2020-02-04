e4f2315ffc31cb26868e80690621eea7acd3e085
fpeyg <frederiqueperrin@gmail.com>
2020-02-04 10:55:38 +0100

    ajout register et index

--- src/main/java/com/a84/quizz/quizz/c/MainController.java
+++ src/main/java/com/a84/quizz/quizz/c/MainController.java
@@ -1,16 +1,61 @@
 package com.a84.quizz.quizz.c;
 
+import com.a84.quizz.quizz.m.User;
+import com.a84.quizz.quizz.m.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @SessionAttributes("grosminet")
 public class MainController {
 
-    @RequestMapping(value = "/login")
+    @Autowired
+    UserDAO userDAO ;
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String register(
+            @RequestParam(name="name", required = true) String name,
+            @RequestParam(name="password", required = true) String password,
+            @RequestParam(name="passwordc", required = true) String passwordc,
+            Model model) {
+// Check if provided passwords are the same
+        if (password.equals(passwordc)) {
+// Check if the name is already used.
+            boolean found = userDAO.existsByName(name);
+            if (found) {
+                model.addAttribute("error_msg", "Error: pick another name.");
+                return "register";
+            }
+// Now we can add the new user.
+            User u = new User();
+            u.setName(name);
+            String salt = BCrypt.gensalt();
+            u.setSalt(salt);
+            u.setHashedPassword(BCrypt.hashpw(password, salt));
+            u.setAccessLevel(0);
+            userDAO.save(u);
+            return "index";
+        }
+        else {
+            model.addAttribute("error_msg", "Error: Passwords don't match.");
+        }
+        return "register";
+    }
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String register() {
+        return "register";
+
+    }
+    @RequestMapping(value = "/index")
     public String login(@RequestParam(name="name", required = false)
                                 String name, Model model) {
+
         if (name != null) {
             model.addAttribute("grosminet", name);
             model.addAttribute("name", name);
@@ -18,6 +63,6 @@ public class MainController {
         else if (model.getAttribute("grosminet") != null) {
             model.addAttribute("name", model.getAttribute("grosminet"));
         }
-        return "login";
+        return "index";
     }
 }
--- src/main/java/com/a84/quizz/quizz/m/UserDAO.java
+++ src/main/java/com/a84/quizz/quizz/m/UserDAO.java
@@ -2,6 +2,11 @@ package com.a84.quizz.quizz.m;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+
 public interface UserDAO extends JpaRepository<User, Integer> {
     boolean existsByName(String name);
+    List<User> findByName(String name);
 }
+
