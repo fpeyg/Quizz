84bdecee64375783a7c4cfa809b73ef137772d02
bilalmourdi <58844248+bilalmourdi@users.noreply.github.com>
2020-02-04 09:54:54 +0100

    /

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,4 +7,47 @@ CREATE TABLE `user` (
   `name` varchar(255) DEFAULT NULL,
   `salt` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
+) ;
+
+DROP TABLE IF EXISTS `resultat_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `resultat_user` (
+  `user_id` int(11) NOT NULL,
+  `scrore` int(11) NOT NULL,
+  `session_user` int(11) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `question` (
+  `question_id` int(11) NOT NULL,
+  `point` int(3) DEFAULT NULL,
+  `wording` TEXT DEFAULT NULL,
+   PRIMARY KEY (`question_id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `choice`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `choice` (
+  `choice_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `scale` int(3) DEFAULT NULL,
+  `anwser` TEXT DEFAULT NULL,
+  `resultat` BOOLEAN,
+  `clue` TEXT DEFAULT NULL,
+   PRIMARY KEY (`choice_id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `answer_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `answer_user` (
+  `user_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `choice_id` int(11) NOT NULL,
+  `answer_question` TEXT,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
